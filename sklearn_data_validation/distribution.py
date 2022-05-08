06dd6ce38e2aca2f47e90897757bee2a291fc7b2
ahmetsirel <31900749+ahmetsirel@users.noreply.github.com>
2022-05-08 18:27:58 +0200

    Develop

--- sklearn_data_validation/distribution.py
+++ sklearn_data_validation/distribution.py
@@ -4,44 +4,56 @@ import metrics
 from scipy.stats import kstest, normaltest, chisquare
 from sklearn.preprocessing import StandardScaler
 
+
 class Distribution:
-    def __init__(self, var: pd.Series, bins: int=100):
+    def __init__(self, var: pd.Series, bins: int = 100, p_threshold=0.05):
         self.bins = bins
         self.pdf_ = var.value_counts(bins=self.bins, normalize=True).sort_index()
         self.cdf_ = self.pdf_.cumsum()
-        
+
         self.dist_q, self.dist_q_bins = pd.qcut(var, self.bins, retbins=True)
         self.dist_q = self.dist_q.value_counts(normalize=True)
-        
-        self.stats =  var.describe(percentiles=np.arange(0 ,1, 1/self.bins)[1:], include='all').to_dict()
+
+        self.dist, self.dist_bins = pd.cut(var, self.bins, retbins=True)
+        self.dist = self.dist.value_counts(normalize=True)
+
+        self.stats = var.describe(percentiles=np.arange(0, 1, 1 / self.bins)[1:], include="all").to_dict()
         for stat_key, stat_value in self.stats.items():
             setattr(self, stat_key, stat_value)
-    
+
+        self.is_var_normal = metrics.is_var_normal(var, p_threshold=p_threshold)
+
     @staticmethod
-    def get_df_value(x, df_):
+    def get_distribution_function_value(x, df_):
         # Get corresponding value from cumulative or probability distributions.
         if x > df_.index.max().right:
             x = df_.index.max().right - 0.00001
         elif x < df_.index.min().left:
             x = df_.index.min().left + 0.00001
         return df_.iloc[df_.index.get_loc(x)]
-    
+
     def cdf(self, x):
-        if hasattr(x, '__iter__'):
+        if hasattr(x, "__iter__"):
             x = [i for i in x]
-            return list(map(lambda x_: self.get_df_value(x_, self.cdf_), x))
+            return list(map(lambda x_: self.get_distribution_function_value(x_, self.cdf_), x))
         else:
-            return self.get_df_value(x, self.cdf_)
-        
+            return self.get_distribution_function_value(x, self.cdf_)
+
     def pdf(self, x):
-        if hasattr(x, '__iter__'):
+        if hasattr(x, "__iter__"):
             x = [i for i in x]
-            return list(map(lambda x_: self.get_df_value(x_, self.pdf_), x))
+            return list(map(lambda x_: self.get_distribution_function_value(x_, self.pdf_), x))
         else:
-            return self.get_df_value(x, self.cdf_)
-        
+            return self.get_distribution_function_value(x, self.cdf_)
+
     def __repr__(self):
         return str(vars(self).keys())
 
-    def kstest(self, var):
-        return metrics.is_ks_successfull(var, dist_ref=self)
+    def calculate_kstest_pvalue(self, var):
+        return metrics.kstest_pvalue(var, ref_dist_cdf=self.cdf)
+
+    def calculate_chisquare_pvalue(self, var):
+        return metrics.chisquare_pvalue(var=var, ref_dist=self.dist_q, ref_dist_bins=self.dist_q_bins)
+
+    def calculate_psi(self, var):
+        return metrics.psi(var=var, ref_dist=self.dist, ref_dist_bins=self.dist_bins)
--- sklearn_data_validation/metrics.py
+++ sklearn_data_validation/metrics.py
@@ -3,57 +3,40 @@ import numpy as np
 from scipy.stats import wasserstein_distance as scipy_wasserstein_distance
 from sklearn.preprocessing import StandardScaler
 from scipy.stats import kstest, normaltest, chisquare
-from distribution import Distribution
 
-def abs_difference_of_means(dist_ref: Distribution, dist: Distribution):
-    return np.abs(dist_ref.mean - dist.mean)
 
-def relative_abs_difference_of_means(dist_ref: Distribution, dist: Distribution):
-    return np.abs(dist_ref.mean - dist.mean) / dist_ref.mean
+# def abs_difference_of_means(dist_ref, dist):
+#     return np.abs(dist_ref.mean - dist.mean)
 
-def is_ks_successfull(var, dist_ref: Distribution, p_threshold=0.05):
-    return kstest(var, dist_ref.cdf).pvalue > p_threshold
 
-def psi(var, dist_ref: Distribution, bins=10):
-    ref_dist, ref_dist_bins = pd.cut(ref_dist, 10, retbins=True)
-    ref_dist = ref_dist.value_counts(normalize=True)
-    
-    var_dist = pd.cut(dist, bins=ref_dist_bins).value_counts(normalize=True)
-    PSI=((var_dist - ref_dist) * np.log(var_dist / ref_dist)).sum()
+# def relative_abs_difference_of_means(dist_ref, dist):
+#     return np.abs(dist_ref.mean - dist.mean) / dist_ref.mean
+
+
+def kstest_pvalue(var, ref_dist_cdf):
+    # Implemented
+    return kstest(var, ref_dist_cdf).pvalue
+
+
+def psi(var, ref_dist, ref_dist_bins):
+    # Implemented
+    var_dist = pd.cut(var, bins=ref_dist_bins).value_counts(normalize=True)
+    PSI = ((var_dist - ref_dist) * np.log(var_dist / ref_dist)).sum()
     return PSI
 
-def is_chisquare_successfull(ref_dist, dist, p_threshold=0.05):
-    ref_dist, ref_dist_bins = pd.qcut(ref_dist, 4, retbins=True)
-    ref_dist = ref_dist.value_counts(normalize=True)
 
-    var_dist = pd.cut(dist, bins=ref_dist_bins).value_counts(normalize=True)
-    
-    return chisquare(var_dist, f_exp=ref_dist).pvalue > p_threshold
+def chisquare_pvalue(var, ref_dist, ref_dist_bins):
+    # Implemented
+    var_dist = pd.cut(var, bins=ref_dist_bins).value_counts(normalize=True)
 
-def cdf(x, dist, bins=10):
-    cdf_ = dist.value_counts(bins=bins, normalize=True).sort_index().cumsum()
-    def get_cdf(x):
-        if x > cdf_.index.max().right:
-            x = cdf_.index.max().right - 0.00001
-        elif x < cdf_.index.min().left:
-            x = cdf_.index.min().left + 0.00001
-        return cdf_.iloc[cdf_.index.get_loc(x)]
-    if hasattr(x, '__iter__'):
-        x = [i for i in x]
-        return list(map(lambda x_: get_cdf(x_), x))
-    else:
-        return get_cdf(x)
-
-def pdf(x, dist, bins=10):
-    pdf_ = dist.value_counts(bins=bins, normalize=True).sort_index()
-    return pdf_.iloc[pdf_.index.get_loc(x)]
+    return chisquare(var_dist, f_exp=ref_dist).pvalue
 
 
-# def is_dist_normal(dist: Distribution, p_threshold=0.05):
-#     dist = StandardScaler().fit_transform(np.array(dist).reshape(-1, 1))
-#     return normaltest(dist).pvalue > p_threshold
+def is_var_normal(var, p_threshold=0.05):
+    var_scaled = StandardScaler().fit_transform(np.array(var).reshape(-1, 1))
+    return normaltest(var_scaled).pvalue > p_threshold
 
-# def relative_wasserstein_varance(dist_ref: Distribution, dist: Distribution):
+# def relative_wasserstein_varance(dist_ref, dist):
 #     return scipy_wasserstein_distance(ref_dist, dist) / ref_dist.mean()
 
 # def is_var_within_z_threshold(ref_dist, dist, z_threshold=3):
